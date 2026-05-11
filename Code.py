085751923544fd8cce4211c0ae855aadb55dfa3d
Schoolcodingstuff <devt23124@wainuiomatahigh.school.nz>
2026-05-12 10:13:45 +1200

    Update Code.py
    I am going to change up the code because I misunderstood the requirements.

--- Code.py
+++ Code.py
@@ -1,14 +1,1 @@
 from lesson_header import *
-
-# At this point I am planning on making my robot detect each cup independently and move accordingly depending on the cup detected.
-while True:
-if cup_1 == True:
- moves.left (1)
-elif cup_2 == True:
- moves.right (1)
-elif cup_3 == True:
- moves.left (1)
-elif cup_4 == True:
- moves.right
-else
- moves.forward (1)
