e4013852ddb0b908d2f71427483e1ca8cd7395cd
Schoolcodingstuff <devt23124@wainuiomatahigh.school.nz>
2026-05-12 11:00:15 +1200

    Update Code.py
    More syntax issues, will fix tomorrow.

--- Code.py
+++ Code.py
@@ -1,24 +1,40 @@
-from lesson_header import *
+#These are just from the student library.
+
+def forward(seconds: float = 0.5, speed: float = None):
+    setup()
+    s = float(BASE_SPEED if speed is None else speed)
+    base._spam(+s, +s, +s, +s, seconds)
+
+def move_left(seconds: float = 0.5, speed: float = None):
+    setup()
+    s = float(BASE_SPEED if speed is None else speed)
+    base._spam(-s, +s, +s, -s, seconds)
+
+#I edited the move_right command.
+
+def move_right(seconds: float = 0.5, speed: float = None):
+    setup()
+    s = float(BASE_SPEED if speed is None else speed)
+    base._spam(+s, -s, -s, +s, seconds)
 
 #I'm going to try write some basics and improve it from there.
 
-def cups():
+cup = 0
 
- cups = 0
 while cups < 4:
- distance = 10
  distance_cm = sonar.get_distance_cm(filtered=True)
  if distance > 10:
-  moves.forward
+  forward(1, 1)
  elif cups == 1:
-  moves.right(1.0)
+  move_right(1, 1)
  elif cups == 2:
-  moves.left(1.0)
+   move_left(1, 1)
  elif cups == 3:
-  moves.right(1.0)
+   move_right(1, 1)
  elif cups == 4:
-  moves.left(1.0)
+   move_left(1, 1)
  else:
   cups += 1
 
-moves.forward(1.0)
+forward(1, 1)
+
