838acebd5da7244b99977f7a4c4585f1564c495d
Schoolcodingstuff <devt23124@wainuiomatahigh.school.nz>
2026-05-13 15:16:18 +1200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,25 +1,25 @@
 from lesson_header import *
 
-#I'm going to try write some basics and improve it from there.
+#The basics are done, I'm now going to start on improvements.
  
 cups = 0
 
 while cups < 4:
  distance_cm = sonar.get_distance_cm(filtered=True)
  print(f"distance {distance_cm} cups {cups}")
- if distance_cm > 15:
+ if distance_cm > 20:
   moves.forward(0.3)
  elif cups == 0:
-  cups += 1
   moves.move_right(1)
+  cups += 1
  elif cups == 1:
-  cups += 1
   moves.move_left(1)
+  cups += 1 
  elif cups == 2:
-  cups += 1
   moves.move_right(1)
- elif cups == 3:
   cups += 1
+ elif cups == 3:
   moves.move_left(1)
-  
+  cups += 1
+
 moves.forward(1)
