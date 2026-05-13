e5b900bd9c85075eb169e07fa734aca8311994b1
Schoolcodingstuff <devt23124@wainuiomatahigh.school.nz>
2026-05-13 14:20:13 +1200

    Update Code.py
    Got rid of the code from the student library and fixed some bugs.

--- Code.py
+++ Code.py
@@ -1,40 +1,23 @@
-#These are just from the student library.
-
-def forward(seconds: float = 0.5, speed: float = None):
-    setup()
-    s = float(BASE_SPEED if speed is None else speed)
-    base._spam(+s, +s, +s, +s, seconds)
-
-def move_left(seconds: float = 0.5, speed: float = None):
-    setup()
-    s = float(BASE_SPEED if speed is None else speed)
-    base._spam(-s, +s, +s, -s, seconds)
-
-#I edited the move_right command.
-
-def move_right(seconds: float = 0.5, speed: float = None):
-    setup()
-    s = float(BASE_SPEED if speed is None else speed)
-    base._spam(+s, -s, -s, +s, seconds)
+from lesson_header import *
 
 #I'm going to try write some basics and improve it from there.
-
-cup = 0
+ 
+cups = 0
 
 while cups < 4:
  distance_cm = sonar.get_distance_cm(filtered=True)
- if distance > 10:
-  forward(1, 1)
+ print(f"distance {distance_cm} cups {cups}")
+ if distance_cm > 10:
+  moves.forward(0.3)
  elif cups == 1:
-  move_right(1, 1)
+  moves.move_right(1)
  elif cups == 2:
-   move_left(1, 1)
+   moves.move_left(1)
  elif cups == 3:
-   move_right(1, 1)
+   moves.move_right(1)
  elif cups == 4:
-   move_left(1, 1)
+   moves.move_left(1)
  else:
   cups += 1
 
-forward(1, 1)
-
+moves.forward(1)
