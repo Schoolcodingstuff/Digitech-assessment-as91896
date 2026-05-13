81a4117d8a1d3a6df882a809e3b1cd90d7962158
Schoolcodingstuff <devt23124@wainuiomatahigh.school.nz>
2026-05-13 14:58:28 +1200

    Update Code.py
    Small change, mostly just because I think it looks better.

--- Code.py
+++ Code.py
@@ -10,16 +10,16 @@ while cups < 4:
  if distance_cm > 15:
   moves.forward(0.3)
  elif cups == 0:
-  moves.move_right(1)
   cups += 1
+  moves.move_right(1)
  elif cups == 1:
+  cups += 1
   moves.move_left(1)
-  cups += 1 
  elif cups == 2:
+  cups += 1
   moves.move_right(1)
-  cups += 1
  elif cups == 3:
-  moves.move_left(1)
   cups += 1
-
+  moves.move_left(1)
+  
 moves.forward(1)
