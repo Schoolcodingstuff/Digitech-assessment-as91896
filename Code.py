d6bedb94a7dcb6baa448e85ab175c48fdec37bfc
Schoolcodingstuff <devt23124@wainuiomatahigh.school.nz>
2026-05-13 14:29:24 +1200

    Update Code.py
    This should (hopefully) make it through the course without any more issues.

--- Code.py
+++ Code.py
@@ -7,17 +7,19 @@ cups = 0
 while cups < 4:
  distance_cm = sonar.get_distance_cm(filtered=True)
  print(f"distance {distance_cm} cups {cups}")
- if distance_cm > 10:
+ if distance_cm > 15:
   moves.forward(0.3)
  elif cups == 1:
   moves.move_right(1)
+  cups += 1
  elif cups == 2:
-   moves.move_left(1)
+  moves.move_left(1)
+  cups += 1 
  elif cups == 3:
-   moves.move_right(1)
+  moves.move_right(1)
+  cups += 1
  elif cups == 4:
-   moves.move_left(1)
- else:
+  moves.move_left(1)
   cups += 1
 
 moves.forward(1)
