2911b728000abdf93488203716e5cf1d65307ee9
Schoolcodingstuff <devt23124@wainuiomatahigh.school.nz>
2026-05-13 15:39:18 +1200

    Update Code.py
    Lots of improvements to the code.

--- Code.py
+++ Code.py
@@ -3,24 +3,25 @@ from lesson_header import *
 #The basics are done, I'm now going to start on improvements.
  
 cups = 0
+#Implemented the dictionary.
+dict_decision = {
+    0: "right",
+    1: "left",
+    2: "right",
+    3: "left"
+}
 
 while cups < 4:
  distance_cm = sonar.get_distance_cm(filtered=True)
  print(f"distance {distance_cm} cups {cups}")
  if distance_cm > 20:
   moves.forward(0.5)
- elif cups == 0:
+ elif dict_decision[cups] == "right":
   moves.move_right(1.5)
   cups += 1
- elif cups == 1:
-  moves.move_left(1.75)
+ elif  dict_decision[cups] == "left":
+  moves.move_left(2)
   cups += 1 
- elif cups == 2:
-  moves.move_right(1.5)
-  cups += 1
- elif cups == 3:
-  moves.move_left(1.75)
-  cups += 1
 
 moves.forward(2)
 print("Finished")
